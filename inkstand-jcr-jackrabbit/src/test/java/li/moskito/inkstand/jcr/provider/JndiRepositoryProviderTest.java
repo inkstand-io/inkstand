29d39a05fd9617a42a6fbeac221086af76684000
Gerald <gerald.muecke@gmail.com>
2014-12-01 22:42:22 +0100

    IC-13 added unit tests

--- inkstand-jcr-jackrabbit/src/test/java/li/moskito/inkstand/jcr/provider/JndiRepositoryProviderTest.java
+++ inkstand-jcr-jackrabbit/src/test/java/li/moskito/inkstand/jcr/provider/JndiRepositoryProviderTest.java
@@ -1,17 +1,31 @@
 package li.moskito.inkstand.jcr.provider;
 
+import static org.junit.Assert.assertEquals;
+
+import javax.jcr.Repository;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 public class JndiRepositoryProviderTest {
 
+    @Mock
+    private Repository repository;
+
+    @InjectMocks
+    private JndiRepositoryProvider subject;
+
     @Before
     public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
     }
 
     @Test
     public void testGetRepository() throws Exception {
-        throw new RuntimeException("not yet implemented");
+        assertEquals(repository, subject.getRepository());
     }
 
 }
--- inkstand-ms/src/test/java/li/moskito/inkstand/ServiceLauncherTest.java
+++ inkstand-ms/src/test/java/li/moskito/inkstand/ServiceLauncherTest.java
@@ -1,27 +1,44 @@
 package li.moskito.inkstand;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import org.jboss.weld.environment.se.events.ContainerInitialized;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 public class ServiceLauncherTest {
 
+    @Mock
+    private MicroService ms;
+
+    @InjectMocks
+    private ServiceLauncher subject;
+
     @Before
     public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
     }
 
     @Test
     public void testInit() throws Exception {
-        throw new RuntimeException("not yet implemented");
+        this.subject.init();
+        verify(ms).start();
     }
 
     @Test
     public void testWatch() throws Exception {
-        throw new RuntimeException("not yet implemented");
+        this.subject.watch(mock(ContainerInitialized.class));
+        // nothing happens here, watch is just for the Standalone Weld Launcher
     }
 
     @Test
     public void testShutdown() throws Exception {
-        throw new RuntimeException("not yet implemented");
+        this.subject.shutdown();
+        verify(ms).stop();
     }
 
 }
