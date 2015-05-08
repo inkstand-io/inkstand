c5a342374b176c8f56c8f2ef392f78e7b3e1624c
Gerald Mücke <gerald.muecke@gmail.com>
2015-05-08 22:21:22 +0200

    INK-29 fixed sonar issue

--- inkstand-deployments/inkstand-deployment-resteasy/src/main/java/io/inkstand/deployment/resteasy/BasicSecurityResteasyDeploymentProvider.java
+++ inkstand-deployments/inkstand-deployment-resteasy/src/main/java/io/inkstand/deployment/resteasy/BasicSecurityResteasyDeploymentProvider.java
@@ -16,12 +16,6 @@
 
 package io.inkstand.deployment.resteasy;
 
-import io.undertow.Undertow;
-import io.undertow.servlet.Servlets;
-import io.undertow.servlet.api.DeploymentInfo;
-import io.undertow.servlet.api.ListenerInfo;
-import io.undertow.servlet.api.ServletInfo;
-
 import javax.annotation.Priority;
 import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
@@ -30,11 +24,14 @@ import javax.inject.Singleton;
 import io.inkstand.ProtectedService;
 import io.inkstand.config.ApplicationConfiguration;
 import io.inkstand.http.undertow.UndertowDeploymentProvider;
-
+import io.undertow.Undertow;
+import io.undertow.servlet.Servlets;
+import io.undertow.servlet.api.DeploymentInfo;
+import io.undertow.servlet.api.ListenerInfo;
+import io.undertow.servlet.api.ServletInfo;
 import org.jboss.resteasy.cdi.CdiInjectorFactory;
 import org.jboss.resteasy.plugins.server.servlet.HttpServlet30Dispatcher;
 import org.jboss.resteasy.spi.ResteasyDeployment;
-
 import ws.ament.hammock.core.impl.CDIListener;
 
 /**
@@ -48,7 +45,7 @@ import ws.ament.hammock.core.impl.CDIListener;
 @Priority(0)
 public class BasicSecurityResteasyDeploymentProvider implements UndertowDeploymentProvider {
 
-    // TODO make security configuraiton configurable
+    // TODO make security configuration configurable
 
     @Inject
     private ApplicationConfiguration appConfig;
@@ -56,8 +53,6 @@ public class BasicSecurityResteasyDeploymentProvider implements UndertowDeployme
     @Override
     @Produces
     public DeploymentInfo getDeployment() {
-
-        // new UndertowJaxrsServer();
 
         final ResteasyDeployment deployment = new ResteasyDeployment();
         deployment.getActualResourceClasses().addAll(appConfig.getResourceClasses());
