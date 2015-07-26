526541f04a6dbfceb34256665d35cebef7dd86a2
Gerald Mücke <gerald.muecke@gmail.com>
2015-07-26 21:33:14 +0200

    INK-47 Support serving static content from a zip archive via HTTP
    - added IT case stub

---
+++ inkstand-it/inkstand-it-staticcontent/src/test/java/io/inkstand/it/StaticContentITCase.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2015 Gerald Muecke, gerald.muecke@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.inkstand.it;
+
+import javax.ws.rs.client.ClientBuilder;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+import org.apache.commons.io.IOUtils;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import io.inkstand.Inkstand;
+import io.inkstand.scribble.Scribble;
+import io.inkstand.scribble.net.NetworkUtils;
+
+/**
+ * Test case for testing serving of static content provided in a zip file.
+ * Created by Gerald on 26.07.2015.
+ */
+public class StaticContentITCase {
+
+    public final static TemporaryFolder folder = Scribble.newTempFolder().build();
+
+    public static File contentFile;
+
+    private int port;
+
+
+    @BeforeClass
+    public static void setupTestContent() throws IOException {
+
+        contentFile = createZip(folder.newFile("content.zip"), "/index.html", "/img/image.png");
+
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        port = NetworkUtils.findAvailablePort();
+        //we set the port to use a randomized port for testing, otherwise the default port 80 will be used
+        System.setProperty("inkstand.http.port", String.valueOf(port));
+
+    }
+
+    private static File createZip(final File file, final String... resources) throws IOException {
+
+        try(ZipOutputStream zos = new ZipOutputStream(new FileOutputStream((file)))) {
+
+            for(String resourceName  : resources){
+
+                final InputStream is = StaticContentITCase.class.getResourceAsStream(resourceName);
+                final ZipEntry zipEntry = new ZipEntry(resourceName);
+                zos.putNextEntry(zipEntry);
+                IOUtils.copy(is, zos);
+                zos.closeEntry();
+            }
+        }
+        return file;
+    }
+
+    @Test
+    public void testGetStaticContent_contextRoot_indexFile() {
+        //prepare
+        System.setProperty("inkstand.http.content.zip", contentFile.getAbsolutePath());
+        Inkstand.main(new String[] {});
+
+        //act
+        String value = ClientBuilder.newClient()
+                                    .target("http://localhost:" + port + "/")
+                                    .request().get(String.class);
+        //assert
+        Assert.assertEquals("test", value);
+
+    }
+
+
+}
