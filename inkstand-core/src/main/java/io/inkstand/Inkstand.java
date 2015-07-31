e04c757bc954821e105eca27f36be88af0cb9686
Gerald Mücke <gerald.muecke@gmail.com>
2015-08-01 00:16:35 +0200

    INK-53 HttpServer Launcher
    - added basic extensibility for launcher cmd line parameters

--- inkstand-core/src/main/java/io/inkstand/Inkstand.java
+++ inkstand-core/src/main/java/io/inkstand/Inkstand.java
@@ -16,16 +16,76 @@
 
 package io.inkstand;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ServiceLoader;
+
 /**
- * Inkstand launcher. Alternative way of starting Weld SE Container.
+ * Inkstand launcher. It's not just an alternative way of starting the CDI Container. The launcher also provides
+ * an extensible mechanism for processing command line arguments. The command line argument have to be of the format
+ * <code>-[name] [value]/code>, with value being optional (for flag-arguments). The mechanism can be extended
+ * dynamically by modules on the classpath that provide their own implementation of the {@link LauncherArgs} interface
+ * and register them as service. The Inkstand launcher will search those extensions and passed the matching cmd line
+ * arguments for processing to them.
  * 
  * @author <a href="mailto:gerald@inkstand.io">Gerald M&uuml;cke</a>
  */
 public final class Inkstand {
+
     private Inkstand() {
     }
 
     public static void main(final String[] args) {
+
+        //get the name value pairs from the arguments
+        final Map<String, String> argValues = argsToMap(args);
+        applyArgs(argValues);
+
         org.jboss.weld.environment.se.StartMain.main(args);
+    }
+
+    /**
+     * Applies the name-value pairs of arguments to the registered extensions
+     * @param argValues
+     *  the argValues as key-value map. Values may be {@code null}.
+     */
+    private static void applyArgs(final Map<String, String> argValues) {
+
+        final ServiceLoader<LauncherArgs> launcherArgsSPIs = ServiceLoader.load(LauncherArgs.class);
+        for(LauncherArgs launcherArgSPI : launcherArgsSPIs){
+            //TODO refactor two nested for-loops
+            for(String argName : launcherArgSPI.getArgNames()) {
+                if(argValues.containsKey(argName)){
+                    launcherArgSPI.apply(argName, argValues.get(argName));
+                }
+            }
+        }
+    }
+
+    /**
+     * Parses the cmd line arguments into a map. If the arguments starts with a '-' it denotes the argument name. If
+     * the following argument does not start with a '-' it's the corresponding value. If not, the value is {@code null}.
+     * @param args
+     *  the cmd line arguments
+     * @return
+     *  the arguments as a name-value map.
+     */
+    private static Map<String, String> argsToMap(final String[] args) {
+
+        final Map<String, String> argValues = new HashMap<>();
+        for(int i = 0; i < args.length; i++){
+
+            //TODO refactor two nested ifs
+            if(args[i].startsWith("-")){
+                String argName = args[i].substring(1);
+                String argValue = null;
+                if(i+1 < args.length && !args[i+1].startsWith("-")){
+                    argValue = args[i+1];
+                    i++;
+                }
+                argValues.put(argName, argValue);
+            }
+        }
+        return argValues;
     }
 }
---
+++ inkstand-core/src/test/java/io/inkstand/InkstandTest.java
@@ -0,0 +1,148 @@
+/*
+ * Copyright 2015 Gerald Muecke, gerald.muecke@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.inkstand;
+
+import static org.hamcrest.core.IsNull.nullValue;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assume.assumeThat;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Map;
+import java.util.ServiceLoader;
+import org.jboss.weld.environment.se.StartMain;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * Created by Gerald on 31.07.2015.
+ */
+public class InkstandTest {
+
+    private static PrintStream originalOutStream;
+    private static ByteArrayOutputStream baos;
+
+    @BeforeClass
+    public static void setUpSystemOut() throws Exception {
+        originalOutStream = System.out;
+        //prepare the sysout stream to record the console output
+        //in order to record the system out, the output for the console has to be redirected. The console output
+        //is written using an SLF4J logger, see log4j2.xml config for the console output configuration
+        baos = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(baos);
+        System.setOut(ps);
+    }
+
+    @AfterClass
+    public static void tearDownSystemOut() throws Exception {
+        System.setOut(originalOutStream);
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        StartMain.PARAMETERS = null;
+        baos.reset();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        StartMain.PARAMETERS = null;
+        TestLauncherArgs.reset();
+    }
+
+    /**
+     * The test verifies that the TestLauncherArgs is not called to apply any argument (as there is none) and
+     * that the Weld CDI container ist started using the {@link StartMain} .
+     * @throws Exception
+     */
+    @Test
+    public void testMain_noArgs() throws Exception {
+
+        //prepare
+        assumeThat(StartMain.PARAMETERS, nullValue());
+        String[] args = new String[0];
+
+        //act
+        Inkstand.main(args);
+
+        //assert
+        assertArrayEquals(args, StartMain.getParameters());
+        assertFalse(TestLauncherArgs.isApplyInvoked());
+
+    }
+
+    /**
+     * The test verifies that the ? arg is applied to the HelpLauncherArgs which will print out all descriptions
+     * of all LauncherArgs, including the TestLauncherArgs.
+     * @throws Exception
+     */
+    @Test
+    public void testMain_helpArgs() throws Exception {
+
+        assumeThat(StartMain.PARAMETERS, nullValue());
+        //prepare
+        //pass the help arg
+        String[] args = new String[]{"-?"};
+
+        //act
+        Inkstand.main(args);
+
+        //assert
+        assertArrayEquals(args, StartMain.getParameters());
+
+        //the help launcher arg should display the description of all launcherArg extensions on system out
+        //so the output should contain the description of the TestLauncherArgs extension
+        System.setOut(originalOutStream);
+        final String consoleOut = new String(baos.toByteArray());
+        System.out.println(consoleOut);
+        assertTrue(consoleOut.contains("testDescription"));
+
+
+    }
+
+    /**
+     * The test verifies that the TestLauncherArgs is not called to apply any argument (as there is none) and
+     * that the Weld CDI container ist started using the {@link StartMain} .
+     * @throws Exception
+     */
+    @Test
+    public void testMain_applyArgs() throws Exception {
+        assumeThat(StartMain.PARAMETERS, nullValue());
+
+        //prepare
+        String[] args = new String[]{"-test", "testvalue"};
+
+        //act
+        Inkstand.main(args);
+
+        //assert
+        assertArrayEquals(args, StartMain.getParameters());
+        final ServiceLoader<LauncherArgs> launcherArgs = ServiceLoader.load(LauncherArgs.class);
+        assertTrue(TestLauncherArgs.isApplyInvoked());
+        final Map<String,String> appliedArgs = TestLauncherArgs.getAppliedArgs();
+        assertTrue(appliedArgs.containsKey("test"));
+        assertEquals("testvalue", appliedArgs.get("test"));
+
+    }
+
+}
