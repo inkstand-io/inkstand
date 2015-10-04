cfb4a035231e99b0da4175a738a8c119c4775985
Gerald Mücke <gerald.muecke@devcon5.io>
2015-10-04 10:33:19 +0200

    INK-14 Management API on separate port
    - extended jax-rs jmx resource

--- inkstand-deployments/inkstand-mgmt-jmx/src/main/java/io/inkstand/mgmt/jmx/JMX.java
+++ inkstand-deployments/inkstand-mgmt-jmx/src/main/java/io/inkstand/mgmt/jmx/JMX.java
@@ -1,15 +1,19 @@
 package io.inkstand.mgmt.jmx;
 
+import static javax.json.Json.createArrayBuilder;
 import static javax.json.Json.createObjectBuilder;
 
+import javax.json.JsonArrayBuilder;
 import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-
 import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
+import java.lang.management.MemoryUsage;
 import java.lang.management.RuntimeMXBean;
+import java.util.Map;
 
 import io.inkstand.Management;
 
@@ -42,15 +46,56 @@ public class JMX {
         builder.add("bootClassPath", runtime.getBootClassPath());
         builder.add("classPath", runtime.getClassPath());
         builder.add("libraryPath", runtime.getLibraryPath());
-
+        builder.add("managementSpecVersion", runtime.getManagementSpecVersion());
+        builder.add("name", runtime.getName());
+        builder.add("specName", runtime.getSpecName());
+        builder.add("specVersion", runtime.getSpecVersion());
+        builder.add("startTime", runtime.getStartTime());
+        builder.add("upTime", runtime.getUptime());
+        builder.add("vmName", runtime.getVmName());
+        builder.add("vmVendor", runtime.getVmVendor());
+        builder.add("vmVersion", runtime.getVmVersion());
+        builder.add("inputArgs", getInputArgs(runtime));
+        builder.add("systemProperties", getSystemProperties(runtime));
         return builder.build().toString();
     }
 
-    @GET
-    @Path("/memory")
-    public JsonObject memory() {
+    private JsonArrayBuilder getInputArgs(final RuntimeMXBean runtime) {
 
-        return null;
+        JsonArrayBuilder inputArgs = createArrayBuilder();
+        for(String inputArg : runtime.getInputArguments()) {
+            inputArgs.add(inputArg);
+        }
+        return inputArgs;
+    }
+
+    private JsonObjectBuilder getSystemProperties(final RuntimeMXBean runtime) {
+
+        JsonObjectBuilder sysProps = createObjectBuilder();
+        for(Map.Entry<String, String> sysprop : runtime.getSystemProperties().entrySet()){
+            sysProps.add(sysprop.getKey(), sysprop.getValue());
+        }
+        return sysProps;
+    }
+
+    @GET
+    @Produces("application/json")
+    @Path("/memory")
+    public String memory() {
+        final JsonObjectBuilder builder = createObjectBuilder();
+        MemoryMXBean memory = ManagementFactory.getMemoryMXBean();
+        builder.add("heapUsage", toJsonObject(memory.getHeapMemoryUsage()));
+        builder.add("nonHeapUsage", toJsonObject(memory.getHeapMemoryUsage()));
+        return builder.build().toString();
+    }
+
+    private JsonObjectBuilder toJsonObject(final MemoryUsage memUsage) {
+        final JsonObjectBuilder builder = createObjectBuilder();
+        builder.add("init", memUsage.getInit());
+        builder.add("used", memUsage.getUsed());
+        builder.add("max", memUsage.getMax());
+        builder.add("committed", memUsage.getCommitted());
+        return builder;
     }
 
     @GET
